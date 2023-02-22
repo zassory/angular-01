ecec5fd02649321a0fa041168b761f7d7d6c3e9b
zassory <nicolas.programador@gmail.com>
2023-02-22 14:48:31 -0300

    event bindig

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  nombre = 'Nicolas';
-  textoPlaceholder = 'Escriba algo aqui';
-  deshabilitado = true;
-  imgSrc = 'https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg';
+  texto = 'Esto es un video sobre event binding';
+  // nombre = 'Nicolas';
+  // textoPlaceholder = 'Escriba algo aqui';
+  // deshabilitado = true;
+  // imgSrc = 'https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg';
 
   // constructor(){
   //  setInterval(()=> this.nombre = 'Megaman',3000);
   // }
 
   constructor(){
-    setInterval(()=> this.deshabilitado = false,3000);
+    //setInterval(()=> this.deshabilitado = false,3000);
    }
 
   getSuma(numero1:number,numero2:number){
     return numero1 + numero2;
+  }
+
+  cambiarTexto(): void {
+    this.texto = 'En el proximo video vamos a ver Two way data-binding';
   }
 
 }
