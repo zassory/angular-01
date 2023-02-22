bff8a2b0505e12c93da7c1e3be769f62d97a58c2
zassory <nicolas.programador@gmail.com>
2023-02-22 14:15:45 -0300

    Interpolacion ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HolaMundo';
+  nombre = 'Nicolas';
+  textoPlaceholder = 'Escriba algo aqui';
+  deshabilitado = true;
+
+  constructor(){
+   setInterval(()=> this.nombre = 'Megaman',3000);
+  }
+
+  getSuma(numero1:number,numero2:number){
+    return numero1 + numero2;
+  }
+
 }
